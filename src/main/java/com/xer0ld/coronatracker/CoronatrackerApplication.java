d4a37229029c6034a6aab0ac5b8b22a7de0eacd5
markusteimann <markusteimann13@gmail.com>
2022-11-09 11:23:39 +0800

    Starting

---
+++ src/main/java/com/xer0ld/coronatracker/CoronatrackerApplication.java
@@ -0,0 +1,13 @@
+package com.xer0ld.coronatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronatrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xer0ld/coronatracker/CoronatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xer0ld.coronatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
